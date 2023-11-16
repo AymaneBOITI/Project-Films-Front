6422b8729edf6bd43b6c509abecc06859a27da62
MOULOUD EL ARRAM <maoiloud2002@gmail.com>
2023-11-16 15:49:18 +0100

    5 Card instead of 6

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -51,7 +51,7 @@ const Title = styled.h1`
 
 const MoviesGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 1rem;
   padding: 2rem;
 `;
