a22c38ee7c21cdf125c91fedb762a88e92949610
Hzika <127843983+Hzika@users.noreply.github.com>
2023-11-23 14:33:13 +0100

    localhost to ip

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useQuery, useInfiniteQuery } from 'react-query';
 import { MovieSummary, MovieDetails } from './types';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://172.20.10.2:8080';
 
 export const constructImageUrl = (path?: string): string => {
     return `https://image.tmdb.org/t/p/original/${path}`;
